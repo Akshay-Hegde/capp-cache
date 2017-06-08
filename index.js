47d684b2b471b04847d335bd0181b19350047344
Elad Moshe <elad.moshe@capriza.com>
2017-06-08 11:05:21 +0300

    fix bug with cappCacheManifest load path.

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import manifestManager from "./src/manifestManager";
 (function init() {
     let manifest = window.cappCacheManifest;
     if (manifest === undefined) {
-        manifest = { manifestUrl: "cappCacheManifest.json" };
+        manifest = { manifestUrl: "./cappCacheManifest.json" };
     }
     if (manifest.manifestUrl !== undefined) {
         manifestManager.fetchManifest(manifest.manifestUrl).then(({ manifest, wasModified }) => {
             if (wasModified) {
-                load(manifest, true).then(() => trigger("manifestUpdated"));
+                load(manifest, {syncCacheOnly: true}).then(() => trigger("manifestUpdated"));
             }
         });
     } else {
