47960f1db192c40c3b2618eda24e3f7f12e946e1
Elad Moshe <elad.moshe@capriza.com>
2017-06-13 12:18:48 +0300

    improved webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,20 @@ module.exports = function(env = { dev: "true" }) {
         plugins: buildForDev
             ? []
             : [
+		        new webpack.DefinePlugin({
+			        'process.env.NODE_ENV': '"production"'
+		        }),
                   new webpack.optimize.UglifyJsPlugin({
                       output: { comments: false },
                       sourceMap: true,
+	                  mangle: true,
+	                  compress: {
+		                  warnings: false, // Suppress uglification warnings
+		                  pure_getters: true,
+		                  unsafe: true,
+		                  unsafe_comps: true,
+		                  screw_ie8: true
+	                  },
                   }),
               ],
         devServer: {
