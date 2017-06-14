1042721431603321ab830dfab2dc6fc305d9fc06
Elad Moshe <elad.moshe@capriza.com>
2017-06-14 18:16:10 +0300

    re #16 - added polyfill when URL is not available

--- src/id.js
+++ src/id.js
@@ -1,4 +1,16 @@
-export const id = url => {
-	const { href } = new URL(url, window.location.href);
-	return href;
+const polyfillId = url => {
+	const link = document.createElement("a");
+	link.href = url;
+	return link.protocol + "//" + link.host + link.pathname + link.search;
 };
+
+
+export const id = url => {
+	if (window.URL) {
+		const { href } = new URL(url, window.location.href);
+		return href;
+	} else {
+		return polyfillId(url);
+	}
+};
+
