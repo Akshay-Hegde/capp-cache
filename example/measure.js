666a0f38f09a9ff933739eb2d1a382e894dd5700
Elad Moshe <elad.moshe@capriza.com>
2017-06-15 14:32:28 +0300

    added pref measurements to example

--- example/measure.js
+++ example/measure.js
@@ -1,4 +1,4 @@
-window.performance.mark("end");
+window.performance.mark("capp-cache-end");
 window.performance.measure("capp-cache-load", "capp-cache-start", "capp-cache-end");
 const duration = window.performance.getEntriesByName("capp-cache-load", "measure")[0].duration;
 console.log(`Script file was loaded successfully in ${duration} ms`);
