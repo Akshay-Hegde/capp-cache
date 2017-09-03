695ace76238dc2511d31b55ba78914eebdae989d
Elad Moshe <elad.moshe@capriza.com>
2017-09-03 17:26:42 +0300

    fix #49 - Capp-cache fails on Edge browser InPrivate browsing

--- src/indexedDB.js
+++ src/indexedDB.js
@@ -1,1 +1,11 @@
-export default window.indexedDB;
+export default window.indexedDB || {
+  open: () => {
+    const req = {};
+    setTimeout(() =>
+      req.onerror(
+        "No indexedDB on window. This is a known Edge/IE bug, see https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/9968399 "
+      )
+    );
+    return req;
+  },
+};
